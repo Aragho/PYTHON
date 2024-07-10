1bd7099736f23482643a811006802c52cec654d2
Aragho <Oluwadanny19@gmail.com>
2024-07-10 01:35:20 +0100

    commit

---
+++ Chapter2i/assignment11.py
@@ -0,0 +1,27 @@
+first_digit_integer = int(input('enter a digit:'))
+second_digit_integer = int(input('enter a digit:'))
+third_digit_integer = int(input('enter a digit:'))
+fourth_digit_integer = int(input('enter a digit:'))
+fifth_digit_integer = int(input('enter a digit:'))
+
+print(f'{digit} \t, ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
---
+++ Chapter2i/assignment7.py
@@ -0,0 +1,10 @@
+
+if 10 % 2 == 0:
+	print("2 is a multiple of 10")
+elif 10 % 2 != 0:
+	print("2 is not a multiple of 10")
+
+if 1024 % 4 == 0:
+	print("1024 is a multiple of 4")
+elif 1024 % 4 != 0:
+	print("1024 is not a multiple of 4")
