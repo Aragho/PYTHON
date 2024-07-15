771e64732c4c619b6ecd671edb4bfeb84f285ee9
Aragho <Oluwadanny19@gmail.com>
2024-07-15 06:38:52 +0100

    commit

--- Assignment/multiplicationtable.py
+++ Assignment/multiplicationtable.py
@@ -1,5 +1,5 @@
-user_input = int(input('Enter a number to check the multipplication table: '))
+number = int(input('Enter a number to check the multipplication table: '))
 
-for row in range(10):
-	print(user_input, "x" , range, "=" ,user_input * range )
+for row in range(11):
+	print(f"{number} X {row} = {number * row}")
 
