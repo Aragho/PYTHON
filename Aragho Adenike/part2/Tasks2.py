bc7fe7a1fa7050ae3c460a4e4e84e0b01b92af74
Aragho <Oluwadanny19@gmail.com>
2024-07-30 11:40:43 +0100

    code

--- Aragho Adenike/part2/Tasks2.py
+++ Aragho Adenike/part2/Tasks2.py
@@ -1,9 +1,7 @@
 def reverse(number):
 	integer1 = number % 10
 	integer2 = number // 10
-	integer3 = number % 10
-	integer4 = number // 10
-	result = str(integer1) + str(integer2) + str (integer3) + (integer4)
+	result = str(integer1) + str(integer2)
 	return result
 
 
@@ -12,21 +10,20 @@ def reverse(number):
 def is_palindrome(number):
 	integer1 = number % 10
 	integer2 = number // 10
-	integer3 = number % 10
-	integer4 = number // 10
 	
-	if integer4 == integer1:
+		
+	if integer2 == integer1:
 		result = "true"
-	if integer4  != integer1:
-
+	if integer2  != integer1:
 		result = "false"
 	return result
 
-print(reverse(656))
-print(is_palindrome)
-
 	
 
+
+
+print(reverse(456))
+print(is_palindrome)
 
 
 
