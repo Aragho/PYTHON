5d7cd8528553a732c8cdde36626d8b900f7f27b0
Aragho <Oluwadanny19@gmail.com>
2024-07-30 11:59:41 +0100

    code

--- Aragho Adenike/part2/Tasks3.py
+++ Aragho Adenike/part2/Tasks3.py
@@ -1,35 +1,10 @@
-def even_and_odd(number):
-	for index in range(len(number)):
-		if index % 2 == 0:
-			return number[index]
-		elif index % 2 != 0:
-			return number[index]
+def even_and_odd():
+	even = number % 2 == 0
+	odd = number % 3 == 0
+	if number % 2 == 0:
+		return even
+	else:
+		return odd
 
-integers = input('Enter a number:')
-result = even_and_odd(integers)
-print(f'number,{even number}')
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-def odd_position(number):
-	
-	for index in range (len(number)):
-		if index % 2 != 0:
-			print(number[index])
-	return number[index]
-
-odd = [2, 20, 30, 40, 50, 60, 70, 80]
-position = odd_position(odd)
-print(position)
+number = int(input('Enter a number:'))
+print(even_and_odd())
